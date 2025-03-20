502fceaa68c7f065c0ae47a3e0dd8b969d182a5f
Anton Chernov <chernov@brask.ai>
2025-03-20 19:12:46 +0300

    This package is now pure ESM. Flat config is now required. ESLint 9.20.0 or later is required. Update dependencies

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,29 +1,17 @@
-import type { Linter } from  "eslint";
+import type { Linter } from "eslint";
 
 declare const _default: {
   configs: {
-    flat: {
-      recommended: Linter.Config;
-      common: Linter.Config;
-      import: Linter.Config;
-      sonarjs: Linter.Config;
-      prettier: Linter.Config;
-      react: Linter.Config;
-      reactHooks: Linter.Config;
-      reactRefresh: Linter.Config;
-      jsxA11y: Linter.Config;
-      testingLibrary: Linter.Config;
-      promise: Linter.Config;
-      unicorn: Linter.Config;
-      perfectionist: Linter.Config;
-      typescript: Linter.Config;
-    };
     recommended: Linter.Config;
     common: Linter.Config;
     import: Linter.Config;
     sonarjs: Linter.Config;
     prettier: Linter.Config;
     react: Linter.Config;
+    reactHooks: Linter.Config;
+    reactRefresh: Linter.Config;
+    jsxA11y: Linter.Config;
+    testingLibrary: Linter.Config;
     promise: Linter.Config;
     unicorn: Linter.Config;
     perfectionist: Linter.Config;
